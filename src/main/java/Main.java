574eb981ac200b2d1eee19fa3ca8dfb782b11a0b
shanmukhi113-ops <shanmukhip.113@gmail.com>
2026-06-18 16:53:01 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -19,7 +20,22 @@ public class Main {
                 if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
                     System.out.println(command + " is a shell builtin");
                 } else {
-                    System.out.println(command + ": not found");
+                    String pathEnv = System.getenv("PATH");
+                    String[] directories = pathEnv.split(File.pathSeparator);
+                    boolean found = false;
+                    
+                    for (String dir : directories) {
+                        File file = new File(dir + File.separator + command);
+                        if (file.exists() && file.canExecute()) {
+                            System.out.println(command + " is " + file.getAbsolutePath());
+                            found = true;
+                            break;
+                        }
+                    }
+                    
+                    if (!found) {
+                        System.out.println(command + ": not found");
+                    }
                 }
             } else {
                 System.out.println(input + ": command not found");
