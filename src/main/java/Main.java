51e54f60db0e8c09331f3d63ae73a59a91c040c0
shanmukhi113-ops <shanmukhip.113@gmail.com>
2026-06-18 16:49:45 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,13 @@ public class Main {
                 break;
             } else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
+            } else if (input.startsWith("type ")) {
+                String command = input.substring(5);
+                if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
+                    System.out.println(command + " is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
             } else {
                 System.out.println(input + ": command not found");
             }
